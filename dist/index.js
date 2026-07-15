d089d5a6247d6772342a722b59905a282bc0511a
Emmanuel Prunet <emmanuel.prunet@coderoast.fr>
2026-07-15 18:31:32 +0200

    1.8.0 start

--- dist/index.js
+++ dist/index.js
@@ -98513,7 +98513,7 @@ import { promises as fs11 } from "fs";
 import * as path7 from "path";
 
 // src/sift-version.ts
-var SIFT_VERSION = "1.7.8";
+var SIFT_VERSION = "1.8.0";
 
 // src/resolve-sift.ts
 var RELEASE_REPO = "CodeRoasted/sift-action";
